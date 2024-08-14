933f9d49ce2c6f7fbe5823c33f0674af31dea946
Christian <97022022+Christian-Du@users.noreply.github.com>
2024-08-14 08:47:35 +0200

    Add files via upload

---
+++ create-modules.ps1
@@ -0,0 +1,19 @@
+# Array of module names
+$modules = @("zusammenstellungen")
+
+# Loop through each module name
+foreach ($module in $modules) {
+    # Generate module with routing
+    ng generate module /$module --routing
+
+    # Generate component within the module and 
+    ng generate component /$module/$module --flat --standalone false
+
+     # Generate edit component within the module and flatten the structure
+     ng generate component /$module/$module-edit  --standalone false
+
+     # Generate search component within the module and flatten the structure
+     ng generate component /$module/$module-search --standalone false
+}
+
+Write-Output "Modules and components have been created."
